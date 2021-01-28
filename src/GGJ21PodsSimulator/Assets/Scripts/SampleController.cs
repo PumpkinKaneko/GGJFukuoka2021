47f23f18cf26776a35ca5be062434c9d7ff354e0
majimun <yanbarumajimun@gmail.com>
2021-01-28 23:11:45 +0900

    Unity project commit

---
+++ src/GGJ21PodsSimulator/Assets/Scripts/SampleController.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleController : MonoBehaviourPunCallbacks
+{
+    public float MoveSpeed = 5f;
+    public float RotateSpeed = 90f;
+
+    private Vector3 _axis;
+
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {   // キャラ別に操作させる
+            _axis.x = Input.GetAxis("Horizontal");
+            _axis.y = Input.GetAxis("Vertical");
+
+            this.Move(this.transform.forward * _axis.y * this.MoveSpeed);
+            this.Rot(this.transform.up * _axis.x * this.RotateSpeed);
+        }
+    }
+
+
+    private void Move(Vector3 velocity)
+    {
+        this.transform.position += velocity * Time.deltaTime;
+    }
+
+
+    private void Rot (Vector3 rotate)
+    {
+        this.transform.Rotate(rotate * Time.deltaTime);
+    }
+}
---
+++ src/GGJ21PodsSimulator/Assets/Scripts/SampleLobbyScene.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleLobbyScene : MonoBehaviourPunCallbacks
+{
+    public string RoomName = "ConnectedRoom";
+
+    private void Start()
+    {
+        // マスターサーバーに接続
+        PhotonNetwork.ConnectUsingSettings();
+        
+    }
+
+    
+    /// <summary>
+    /// 接続完了のコールバック
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        // 指定した名称のルームに接続
+        PhotonNetwork.JoinOrCreateRoom(this.RoomName, new RoomOptions(), TypedLobby.Default);
+
+        base.OnConnectedToMaster();
+    }
+
+
+    /// <summary>
+    /// マッチング成功時のコールバック
+    /// </summary>
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Prefabs/SamplePlayer", Vector3.zero, Quaternion.identity);
+
+        base.OnJoinedRoom();
+    }
+}
