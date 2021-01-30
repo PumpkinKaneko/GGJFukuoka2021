c653b1fe80f7c2383eb736a677eead23cdf5fa03
majimun <yanbarumajimun@gmail.com>
2021-01-30 11:58:53 +0900

    GameManager firest update

---
+++ src/GGJ21PodsSimulator/Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T> {
+    private static T instance;
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = (T)FindObjectOfType(typeof(T));
+#if UNITY_EDITOR
+                if (instance == null) {
+                    Debug.LogError(typeof(T) + "is nothing");
+                }
+#endif
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake() {
+        CheckInstance();
+    }
+
+    protected bool CheckInstance() {
+        if (instance == null) {
+            instance = (T)this;
+            return true;
+        } else if (Instance == this) {
+            return true;
+        }
+
+        Destroy(this.gameObject);
+        return false;
+    } 
+}
